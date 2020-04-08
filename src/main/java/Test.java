de61d763d3e7c62ae8eeae1d3ad3a9aa5fa07ad8
cat-inGlasses <nikita.pochapynskyi@gmail.com>
2020-04-08 14:11:49 +0200

    LinkedList introducing

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Test {
@@ -6,12 +7,12 @@ public class Test {
 //        ArrayList<Integer> list = new ArrayList<>();
         // because of convention for collections it is better to create the with the type of inteface
         // but not with the type of class, so it will be:
-        List<Integer> list = new ArrayList<>();
-
-
-        for (int i = 0; i < 10; i++) {
-            list.add(i);
-        }
+//        List<Integer> list = new ArrayList<>();
+//
+//
+//        for (int i = 0; i < 10; i++) {
+//            list.add(i);
+//        }
 
         // step. 1
 //        System.out.println(list);
@@ -29,7 +30,42 @@ public class Test {
 //        }
 
         // step 3
-        list.remove(5);
-        System.out.println(list);
+//        list.remove(5);
+//        System.out.println(list);
+
+        //------------------
+//        Lesson 3: Linked List
+        List<Integer> linkedList = new LinkedList<>();
+        List<Integer> arrayList = new ArrayList<>();
+
+        measureTime(linkedList);
+        measureTime(arrayList);
+    }
+
+    private static void measureTime(List<Integer> list) {
+
+        // 2
+//        for (int i = 0; i < 100000; i++) {
+//            list.add(i);
+//        }
+
+        long start = System.currentTimeMillis();
+
+        // 1
+//        for (int i = 0; i < 1000000; i++) {
+//            list.add(i);
+//        }
+
+        // 2
+//        for (int i = 0; i < 10000; i++){
+//            list.get(i);
+//        }
+
+        for (int i = 0; i < 100000; i++) {
+            list.add(0,i);
+        }
+
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
     }
 }
