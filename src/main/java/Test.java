e65a49dd4058dbe26ce7061cebc1bc6ddbce52f7
cat-inGlasses <nikita.pochapynskyi@gmail.com>
2020-04-08 13:32:09 +0200

    ArrayList introducing

---
+++ src/main/java/Test.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+//        ArrayList<Integer> list = new ArrayList<>();
+        // because of convention for collections it is better to create the with the type of inteface
+        // but not with the type of class, so it will be:
+        List<Integer> list = new ArrayList<>();
+
+
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+
+        // step. 1
+//        System.out.println(list);
+//        System.out.println(list.get(0));
+//        System.out.println(list.get(99));
+//        System.out.println(list.size());
+
+        // step. 2
+//        for (int i = 0; i < list.size(); i++) {
+//            System.out.println(list.get(i));
+//        }
+//
+//        for (Integer i : list) {
+//            System.out.println(i);
+//        }
+
+        // step 3
+        list.remove(5);
+        System.out.println(list);
+    }
+}
