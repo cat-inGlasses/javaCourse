60b14cb5c3074c44ffef9ca646d0d9a04ec4d95b
cat-inGlasses <nikita.pochapynskyi@gmail.com>
2020-04-10 11:51:57 +0200

    Annotation intro.

---
+++ src/main/java/Lesson42.java
@@ -0,0 +1,9 @@
+
+@MethodInfo(purpose = "Print He")
+public class Lesson42 {
+
+    @MethodInfo(author = "Nikitas", dateOfCreation = 2020, purpose = "Print Hello World")
+    public void printHelloWorld() {
+
+    }
+}
